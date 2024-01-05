1936c0309bae3f4a97b0887aeb9ce08cc9c14079
srinageswari <srinageswari.m@gmail.com>
2024-01-05 20:51:43 +0530

    Refactor the modules to accomodate the shareable client module within each service

--- scheduler-service/src/main/java/srinageswari/programmedhousehold/schedulerservice/SchedulerController.java
+++
@@ -1,8 +0,0 @@
-package srinageswari.programmedhousehold.schedulerservice;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1")
-public class SchedulerController {}
